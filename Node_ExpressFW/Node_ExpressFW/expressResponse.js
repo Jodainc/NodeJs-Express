e805cf144a2a4d7cbeb4a592c5ae5c33ab380318
theoverlord <jhosue9611@gmail.com>
2017-05-15 16:45:36 -0500

    Initial commit

---
+++ Node_ExpressFW/Node_ExpressFW/expressResponse.js
@@ -0,0 +1,9 @@
+﻿var express = require('express'),
+    app = express();
+
+app
+    .get('/', (req, res) => {
+        res.end(`<h1> hola mundo }:)  </h1>`)
+    })
+app.get('/user/:id', (req, res) => { res.end(`<h1>bienvenido a express ${req.params.id}</h1>`) })
+    .listen(3000)
---
+++ Node_ExpressFW/Node_ExpressFW/server.js
@@ -0,0 +1,8 @@
+﻿    var express = require('express'),
+    app = express();
+
+app  
+    .get('/', (req, res) => {
+        res.sendFile(`${__dirname}/assets/HTML1.html`)
+    })
+    .listen(3000)
